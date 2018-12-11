7032f57643545dbd52587c7d718c02af8ae4b897
MDaighFullerton <36572267+MDaighFullerton@users.noreply.github.com>
2018-12-10 18:30:40 -0800

    Pass data from event-info to templates

--- savethedate-app/src/app/templates/templates.component.ts
+++ savethedate-app/src/app/templates/templates.component.ts
@@ -1,28 +1,31 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, Input } from '@angular/core';
 import { EventInfoComponent } from '../event-info/event-info.component';
 import { InviteTemplates } from './templateModel';
 import { InfoModel } from '../event-info/infoModel';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 // import { userInfo } from 'os';
 
-@Component({
+  @Component({
   selector: 'app-templates',
   templateUrl: './templates.component.html',
   styleUrls: ['./templates.component.css']
 })
 export class TemplatesComponent implements OnInit {
   // @ViewChild(EventInfoComponent) child: EventInfoComponent;
+  @Input() data;
   @ViewChild(EventInfoComponent, {read: 'emailProvider'}) emailStuff: string;
   showAlert() {
     // this.alert.lastName();
     // console.log('sss', this.child.eventInfoForm);
     // window.alert('show alert', this.eventInfoForm.value.email);
+    console.log('data: ' + this.data);
     console.log('call get', this.emailStuff);
   }
   // InfoModel: any;
   // eventInfoForm: FormGroup;
   constructor() {}
   ngOnInit() {
+    console.log('data: ' + this.data);
     console.log('init template');
     // console.log('getting info model');
     // const invite: InviteTemplates = {
